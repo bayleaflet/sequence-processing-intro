e7b1e12569b898d985e26cfecaeefd9678180a8e
bayleaflet <bayleejchristensen@gmail.com>
2024-01-25 20:23:40 -0800

    Renamed files, a lot of editing
    Renamed Exercise_1_bash.sh or whatever it was called to BashScript... so
    it is easier to find. Also made a ton of edits on PyScript where it is
    properly translating the sequences, and counting the variant sites. Now
    I just need to create a new fasta file with the protein sequences.

--- PyScript_Exercise1.py
+++ PyScript_Exercise1.py
@@ -3,44 +3,44 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 import sys
 
-def main():
-
-    DNA_sequences = []
-    variant_sites_count = 0
-    # could tie this into a function
+def read_dna_sequences():
+    dna_sequences = []
     for line in sys.stdin:
-        #Process each sequence line from stdin and make each sequence a list
-        DNA_sequences.append(list(line.strip()))
+        if line[0] != '>':
+        # Puts all sequences in a List
+            dna_sequences.append(list(line.strip()))
+    return dna_sequences
 
-    # Since all sequences have same length, we find the sequence length for later reference
-    sequence_length = len(DNA_sequences[0])
+def translate_to_amino(dna_sequences):
+    # Had help with this step, I think it puts everything from the list back
+    # into a string, which makes me think twice about putting everything in a
+    # list in the first place. Regardless, Seq makes a Seq object, in which
+    # Bipython's magic turns it into a special object that can be translated.
+    dna_seqs = [Seq(''.join(seq)) for seq in dna_sequences]
+    amino_acid_sequences = [seq.translate() for seq in dna_seqs]
+    return amino_acid_sequences
 
-    # iterate over each position in the sequences
+def count_variant_sites(sequence_list):
+    variant_sites_count =0
+    sequence_length = len(sequence_list[0])
     for i in range(sequence_length):
-        # Looks at every sequence position i for every sequence
-        current_positions = [seq[i] for seq in DNA_sequences]
-
-        # Checks if there is more than one unique character at that position
+        # Creates a list of current_positions containing ith character for each
+        # sequence in sequence_list. Then extracts character at position i in
+        # each sequence.
+        current_positions = [seq[i] for seq in sequence_list]
         if len(set(current_positions)) > 1:
             variant_sites_count += 1
+    return variant_sites_count
 
-    print("Number of variant sites: ", variant_sites_count)
-
-#
-#
-# Stopped here, will need to review more tomorrow
-#
-#
-def TranslateFile(list_of_sequences):
-    for nuc in range(sequence_length):
-        protein_seq = dna_record.seq.translate()
-
-
-    # write translated protein sequences to a new fasta file
-    SeqIO.write(protein_sequences, output_fasta, "fasta")
-
-    return protein_sequences
-
+def main():
+    dna_sequences = read_dna_sequences()
+    amino_acid_sequences = translate_to_amino(dna_sequences)
+    variant_sites_dna = count_variant_sites(dna_sequences)
+    variant_sites_aa = count_variant_sites(amino_acid_sequences)
+    print("Num of variant sites in DNA seq:", variant_sites_dna)
+    print("Num of varaint sites in amino acid seq:", variant_sites_aa)
+    # Pass protein sequence into each line of the fasta file instead of the DNA
+    # sequence. (I believe this is what we are supposed to do?)
 
 if __name__== "__main__":
     main()
