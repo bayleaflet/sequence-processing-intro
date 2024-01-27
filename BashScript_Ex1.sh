62f918a79ec144becde3fabc6302f7ad09b4b897
bayleaflet <bayleejchristensen@gmail.com>
2024-01-26 19:58:57 -0800

    Lots of editing
    First version of V2_Pyscript, with some minor updates to
    BashScript_Ex1. Realized I needed to be making objects with the
    Biopython module.

--- BashScript_Ex1.sh
+++ BashScript_Ex1.sh
@@ -1,7 +1,4 @@
 # BJC Original Author, 01/18/2024
-#
-# Should we be setting variables for the alignment name? what about working
-# directory?
 
 cd '/Users/bayleechristensen/OneDrive - Utah Tech University/SpringClasses2024/Adv_Bioinfo/Week_2/sequence-processing-intro'
 
@@ -11,5 +8,7 @@ grep -c "^>" ExampleAlignment.fasta >> log.txt
 # Reports number of samples with unique dates
 awk -F_ '/_[0-9]{1,2}-[[:alpha:]]{3}/{dates[$2]} END {print length(dates)}' ExampleAlignment.fasta >> log.txt
 
-# Pipes sequences to python script
-grep -v "^>" ExampleAlignment.fasta | python3 Other.py >> log.txt
+# Pass file to python script
+python V2_PyScript.py ExampleAlignment.fasta >> log.txt
+
+
